db8fe81f021ae90fefad18e324bb084c331149d8
Ícaro Sanches <igfsaicaro@gmail.com>
2024-09-27 23:58:02 -0300

    Organização de layout e adição de animações da página Home

--- front/riss/src/app/components/pages/home/home.component.ts
+++ front/riss/src/app/components/pages/home/home.component.ts
@@ -16,6 +16,8 @@ export class HomeComponent implements AfterViewInit{
 
   ngAfterViewInit(): void {
     this.rain();
+    this.techno();
+    this.barRoll();
 
     gsap.registerPlugin(ScrollTrigger);
 
@@ -37,24 +39,24 @@ export class HomeComponent implements AfterViewInit{
 
   rain(): void{
     let amount = 100;
-    let area = document.getElementById('bg-chuva');
+    let area = document.getElementById('bg-main');
     let i = 0;
     while(i < amount){
       let drop = document.createElement("i");
       drop.classList.add("rain-drop");
-      drop.style.height = '100px';
+      drop.style.height = (window.innerHeight * 0.12) + 'px';
       drop.style.position = 'absolute';
       drop.style.zIndex = '-1'
       drop.style.rotate = '-30deg'
       drop.style.borderBottomRightRadius = '10px';
       drop.style.borderBottomLeftRadius = '10px';
       drop.style.background = 'linear-gradient(transparent, var(--rain))';
-      drop.style.animation = `animate-rain 5s linear infinite`;
+      drop.style.animation = `animate-Y 10s linear infinite`;
 
-      let auxX = window.innerWidth * 0.4;
+      let auxX = window.innerWidth * 0.5;
 
       let size = Math.random() * 2;
-      let posX = Math.floor(Math.random() * window.innerWidth) * 1.4;
+      let posX = Math.floor(Math.random() * window.innerWidth) * 1.5;
       let delay = Math.random() * -20;
       let duration = Math.random() * 10;
 
@@ -68,6 +70,102 @@ export class HomeComponent implements AfterViewInit{
     }
   }
 
+  techno(): void{
+    let amount = 50;
+    let area = document.getElementById('bg-projetos-home');
+    let i = 0;
+    while(i < amount){
 
+      let drop = document.createElement("i");
+
+      drop.classList.add("techno-drop");
+      drop.style.height = (window.innerHeight * 0.36) + 'px';
+      drop.style.position = 'absolute';
+      drop.style.zIndex = '-1'
+      drop.style.borderBottomRightRadius = '10px';
+      drop.style.borderBottomLeftRadius = '10px';
+      drop.style.background = 'linear-gradient(transparent, var(--rain))';
+      drop.style.display = 'flex';
+      drop.style.justifyContent = 'center';
+      drop.style.alignItems = 'end';
+
+      let size = Math.random() + 1;
+      let delay = Math.random() * -20;
+      let duration = Math.random() * 15;
+      let posX;
+      let posY;
+
+      if (i % 4 == 3){
+        posX = Math.floor(Math.random() * window.innerWidth);
+        drop.style.left = posX + 'px';
+        drop.style.animation = `animate-Y 5s linear infinite`;
+      } else if (i % 4 == 2){
+        drop.style.rotate = '90deg'
+        posY = Math.floor(Math.random() * window.innerHeight);
+        drop.style.top = posY + 'px';
+        drop.style.animation = `animate-X 5s linear infinite`;
+      } else if (i % 4 == 1){
+        drop.style.rotate = '180deg'
+        posX = Math.floor(Math.random() * window.innerWidth);
+        drop.style.right = posX + 'px';
+        drop.style.animation = `animate-Y 5s linear infinite`;
+      } else {
+        drop.style.rotate = '270deg'
+        posY = Math.floor(Math.random() * window.innerHeight);
+        drop.style.bottom = posY + 'px';
+        drop.style.animation = `animate-X 5s linear infinite`;
+      }
+
+      drop.style.width = 0.2 + size + 'px';
+      drop.style.animationDelay = delay + 's';
+      drop.style.animationDuration = 1 + duration +'s';
+
+      let dropDot = document.createElement("i");
+      dropDot.style.height = (0.2 + size) * 1.5 + 'px';
+      dropDot.style.width = (0.2 + size) * 1.5 + 'px';
+      dropDot.style.padding = (0.2 + size) * 1.5 + 'px';
+      dropDot.style.backgroundColor = 'var(--rain)'
+      dropDot.style.borderRadius = '50%'
+
+      drop.appendChild(dropDot);
+
+      drop.style.shapeRendering;
+
+      area?.appendChild(drop);
+      i++;
+    }
+  }
+
+  barRoll(): void{
+    let amount = 15;
+    let area = document.getElementById('bg-quem-sou-home');
+    let i = 0;
+    while(i < amount){
+
+      let drop = document.createElement("i");
+
+      drop.classList.add("bar-drop");
+      drop.style.height = (Math.random() * (window.innerHeight * 0.02)) + (window.innerHeight * 0.005) + 'px';
+      drop.style.position = 'absolute';
+      drop.style.zIndex = '-1'
+      drop.style.borderBottomRightRadius = '5px';
+      drop.style.borderBottomLeftRadius = '5px';
+      drop.style.background = 'linear-gradient(transparent, var(--rain))';
+      drop.style.animation = `animate-Y 10s reverse infinite`;
+
+      let size = (Math.random() * (window.innerHeight * 0.3)) + (window.innerHeight * 0.05);
+      let posX = Math.floor(Math.random() * window.innerWidth) - (window.innerWidth * 0.1);
+      let delay = Math.random() * -20;
+      let duration = Math.random() * 15;
+
+      drop.style.width = size + 'px';
+      drop.style.left = posX + 'px';
+      drop.style.animationDelay = delay + 's';
+      drop.style.animationDuration = 5 + duration +'s';
+
+      area?.appendChild(drop);
+      i++;
+    }
+  }
 
 }
