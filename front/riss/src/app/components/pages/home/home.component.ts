5e1d7d042eb03417e3d6526f14a891f7be6ff8ff
Ícaro Sanches <igfsaicaro@gmail.com>
2024-10-01 19:26:03 -0300

    Mudanças iniciais para arquivos de efeito de fogos de artificio na página home

--- front/riss/src/app/components/pages/home/home.component.ts
+++ front/riss/src/app/components/pages/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Element } from '@angular/compiler';
 import { AfterViewInit, Component, EventEmitter, Output } from '@angular/core';
-import { gsap } from "gsap";
+import { gsap, random } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { delay } from 'rxjs';
 
 
 @Component({
@@ -16,8 +17,9 @@ export class HomeComponent implements AfterViewInit{
 
   ngAfterViewInit(): void {
     this.rain();
-    this.techno();
     this.barRoll();
+    this.firework();
+    this.techno();
 
     gsap.registerPlugin(ScrollTrigger);
 
@@ -43,7 +45,7 @@ export class HomeComponent implements AfterViewInit{
     let i = 0;
     while(i < amount){
       let drop = document.createElement("i");
-      drop.classList.add("rain-drop");
+
       drop.style.height = (window.innerHeight * 0.12) + 'px';
       drop.style.position = 'absolute';
       drop.style.zIndex = '-1'
@@ -70,15 +72,67 @@ export class HomeComponent implements AfterViewInit{
     }
   }
 
-  techno(): void{
-    let amount = 50;
-    let area = document.getElementById('bg-projetos-home');
+  barRoll(): void{
+    let amount = 15;
+    let area = document.getElementById('bg-quem-sou-home');
     let i = 0;
     while(i < amount){
 
       let drop = document.createElement("i");
 
-      drop.classList.add("techno-drop");
+      drop.style.height = (Math.random() * (window.innerHeight * 0.02)) + (window.innerHeight * 0.005) + 'px';
+      drop.style.position = 'absolute';
+      drop.style.zIndex = '-1'
+      drop.style.borderBottomRightRadius = '5px';
+      drop.style.borderBottomLeftRadius = '5px';
+      drop.style.background = 'linear-gradient(transparent, var(--rain))';
+      drop.style.animation = `animate-Y 10s reverse infinite`;
+
+      let size = (Math.random() * (window.innerHeight * 0.3)) + (window.innerHeight * 0.05);
+      let posX = Math.floor(Math.random() * window.innerWidth) - (window.innerWidth * 0.1);
+      let delay = Math.random() * -20;
+      let duration = Math.random() * 15;
+
+      drop.style.width = size + 'px';
+      drop.style.left = posX + 'px';
+      drop.style.animationDelay = delay + 's';
+      drop.style.animationDuration = 5 + duration +'s';
+
+      area?.appendChild(drop);
+      i++;
+    }
+  }
+
+  firework(): void{
+
+
+
+    let amount = 10;
+
+    let i = 0;
+    while(i < amount){
+      let fire = this.fireTail();
+
+      fire.onanimationend = async () => {
+        console.log(parseInt(fire.style.animationDuration ) * 1000);
+        await delay(parseInt(fire.style.animationDuration ) * 1000);
+        let fireDrop = this.fireExplosion(fire);
+
+        return
+      }
+
+
+      i++;
+    }
+  }
+
+  techno(): void{
+    let amount = 50;
+    let area = document.getElementById('bg-projetos-home');
+    let i = 0;
+    while(i < amount){
+      let drop = document.createElement("i");
+
       drop.style.height = (window.innerHeight * 0.36) + 'px';
       drop.style.position = 'absolute';
       drop.style.zIndex = '-1'
@@ -136,36 +190,120 @@ export class HomeComponent implements AfterViewInit{
     }
   }
 
-  barRoll(): void{
-    let amount = 15;
-    let area = document.getElementById('bg-quem-sou-home');
-    let i = 0;
-    while(i < amount){
+  fireTail(): HTMLElement{
+    let area = document.getElementById('bg-dip-cert-home');
+    let drop = document.createElement("i");
 
-      let drop = document.createElement("i");
+    drop.style.backgroundColor = 'var(--rain)';
+    drop.style.borderRadius = '50%';
+    drop.style.position = 'absolute';
+    drop.style.zIndex = '-1';
 
-      drop.classList.add("bar-drop");
-      drop.style.height = (Math.random() * (window.innerHeight * 0.02)) + (window.innerHeight * 0.005) + 'px';
-      drop.style.position = 'absolute';
-      drop.style.zIndex = '-1'
-      drop.style.borderBottomRightRadius = '5px';
-      drop.style.borderBottomLeftRadius = '5px';
-      drop.style.background = 'linear-gradient(transparent, var(--rain))';
-      drop.style.animation = `animate-Y 10s reverse infinite`;
+    let size = (Math.random() * 10) + 5;
 
-      let size = (Math.random() * (window.innerHeight * 0.3)) + (window.innerHeight * 0.05);
-      let posX = Math.floor(Math.random() * window.innerWidth) - (window.innerWidth * 0.1);
-      let delay = Math.random() * -20;
-      let duration = Math.random() * 15;
+    drop.style.height = size + 'px';
+    drop.style.width = size + 'px';
 
-      drop.style.width = size + 'px';
-      drop.style.left = posX + 'px';
-      drop.style.animationDelay = delay + 's';
-      drop.style.animationDuration = 5 + duration +'s';
+    let delay = Math.random() * -20;
+    let duration = Math.random() * 15;
+    let posX = Math.random() * window.innerWidth;
+    let aux_runX = (Math.random() * window.innerWidth / 5);
+    let runY = (Math.random() * window.innerHeight / 2) + (window.innerHeight / 3);
 
-      area?.appendChild(drop);
-      i++;
+    drop.style.setProperty("--posY", "0px");
+    drop.style.setProperty("--runY", -runY + "px");
+    drop.style.setProperty("--posX", posX + "px");
+
+    if (Math.floor(Math.random() * 2) % 2 == 1){
+      drop.style.setProperty("--runX", posX - aux_runX + "px");
+    } else {
+      drop.style.setProperty("--runX", posX + aux_runX + "px");
     }
+
+    drop.style.setProperty("--width", size + "px");
+    drop.style.setProperty("--height", size + "px");
+
+    drop.style.setProperty("--delay", delay + "s");
+    drop.style.setProperty("--duration", 1 + duration + "s");
+
+    drop.style.left = posX + 'px';
+    drop.style.bottom = '0' ;
+
+    drop.style.animation = 'fireworkX 2.5s 1 ease-in';
+    drop.style.animationDelay = delay + 's';
+    drop.style.animationDuration = 1 + duration +'s';
+
+    drop.classList.add("firework");
+
+    drop.style.shapeRendering;
+
+    area?.appendChild(drop);
+
+    return drop;
   }
+
+  fireExplosion(drop: HTMLElement): HTMLElement{
+    let j = 0;
+
+    let size = (Math.random() * 3) + 2;
+
+    while (j < 10){
+      let dropDot = document.createElement("i");
+
+      dropDot.style.backgroundColor = 'var(--rain)'
+      dropDot.style.borderRadius = '50%'
+      dropDot.style.position = 'absolute';
+      dropDot.style.zIndex = '-1'
+
+      dropDot.style.height = size + 'px';
+      dropDot.style.width = size + 'px';
+
+      dropDot.style.rotate = j * 36 + 'deg';
+
+      let posX = drop.style.getPropertyValue("--runX");
+      let posY = drop.style.getPropertyValue("--runY");
+
+//      let aux_posYIni = drop.style.getPropertyValue("--runY");
+      let aux_posXIni = drop.style.getPropertyValue("--posX");
+
+      let aux_runY = parseFloat(posY.replace(/[^0-9],/g, ''));
+      let runY = ( aux_runY / 10 ) -  parseFloat(posY.replace(/[^0-9],/g, '')) ;
+
+      let aux_posX = parseFloat(posY.replace(/[^0-9],/g, ''));
+      let aux_runX = aux_posX - parseFloat(aux_posXIni.replace(/[^0-9],/g, '')) ;
+
+      let runX
+
+      if (parseFloat(aux_posXIni.replace(/[^0-9],/g, ''))  < aux_runX){
+        runX = ( aux_runX / 10 ) + posX;
+      } else {
+        runX = - ( aux_runX / 10 ) + posX;
+      }
+
+      dropDot.style.setProperty("--posX", posX);
+      dropDot.style.setProperty("--posY", posY);
+      dropDot.style.setProperty("--runX", runX + "px");
+      dropDot.style.setProperty("--runY", -runY + "px");
+
+      dropDot.style.setProperty("--delay", '0s');
+      dropDot.style.setProperty("--duration", '2s');
+
+      dropDot.style.left = posX + 'px';
+      dropDot.style.bottom = posY + 'px';
+
+      dropDot.style.animation = 'fireworkX 2.5s 1 ease-in'
+      dropDot.style.animationDelay = '0s';
+      dropDot.style.animationDuration = '2s';
+
+      dropDot.classList.add("firework");
+
+      drop.appendChild(dropDot);
+
+      j++;
+    }
+
+    return drop;
+  }
+
 
 }
