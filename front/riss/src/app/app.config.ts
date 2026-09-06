cf101d25d5cd886457fea1d15a512025f21a6bd8
Ícaro Sanches <igfsaicaro@gmail.com>
2026-09-06 14:35:20 -0300

    fix: atualizar dependencias

--- front/riss/src/app/app.config.ts
+++ front/riss/src/app/app.config.ts
@@ -1,7 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 
@@ -9,11 +8,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimations(),
     providePrimeNG({
         theme: {
           preset: Aura
-        }
+        },
+        license:
+          'eyJpZCI6ImEwNmVmN2FhLWI3NzQtNGVkYS1iNDIyLTY0MzI5YzczN2I0MiIsInByb2R1Y3QiOiJwcmltZXVpIiwidGllciI6ImNvbW11bml0eSIsInR5cGUiOiJkZXYiLCJpYXQiOjE3ODg2NDMwODIsImV4cCI6MTgyMDE3OTA4Mn0.6QTIBp3MFUIT_ZIRmuGP3xb8XKuT-CEXqQsHzKOWHYUrZTKFD_v-xc1_dWVkydNjNuzkmlbnbug1pMoOk_h_Ag'
       })
     ]
 };
